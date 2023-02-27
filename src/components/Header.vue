c146fb9a02f0196fa4f3a9e6dc264cac3777ae8b
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-02-27 18:23:49 +0100

    created data

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,72 @@
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      logo: {
+        src: "",
+        title: "",
+      },
+      links: [
+        {
+          link: "#",
+          text: "CHARACTERS",
+          active: "false",
+        },
+        {
+          link: "#",
+          text: "COMICS",
+          active: "false",
+        },
+        {
+          link: "#",
+          text: "MOVIES",
+          active: "false",
+        },
+        {
+          link: "#",
+          text: "TV",
+          active: "false",
+        },
+        {
+          link: "#",
+          text: "GAMES",
+          active: "false",
+        },
+        {
+          link: "#",
+          text: "COLLECTIBLES",
+          active: "false",
+        },
+        {
+          link: "#",
+          text: "VIDEOS",
+          active: "false",
+        },
+        {
+          link: "#",
+          text: "FANS",
+          active: "false",
+        },
+        {
+          link: "#",
+          text: "NEWS",
+          active: "false",
+        },
+        {
+          link: "#",
+          text: "SHOP",
+          active: "false",
+        },
+      ],
+    };
+  },
+};
+</script>
 
 <template>
-  <h1>dfghfdghfsd</h1>
+  <ul v-for="link in links">
+    <li></li>
+  </ul>
 </template>
 
 <style scoped></style>
